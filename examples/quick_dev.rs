c6cd5d2580a57cebef39858c7018b64a017c2f1a
Salman Khan <salmankkhan733@gmail.com>
2023-11-11 21:09:51 -0500

    Added tests

---
+++ examples/quick_dev.rs
@@ -0,0 +1,10 @@
+use anyhow::{Ok, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:8080")?;
+
+    hc.do_get("/hello").await?.print().await?;
+
+    Ok(())
+}
