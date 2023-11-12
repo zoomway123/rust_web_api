72f3fa1be60b6cc22d8789f54c983d53bd142ff5
Salman Khan <salmankkhan733@gmail.com>
2023-11-11 20:14:21 -0500

    change name

---
+++ target/debug/deps/rust_web_api.d
@@ -0,0 +1,5 @@
+C:\Users\salma\Projects\rust_web_api\target\debug\deps\rust_web_api.exe: src\main.rs
+
+C:\Users\salma\Projects\rust_web_api\target\debug\deps\rust_web_api.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/rust_web_api.d
@@ -0,0 +1,1 @@
+C:\Users\salma\Projects\rust_web_api\target\debug\rust_web_api.exe: C:\Users\salma\Projects\rust_web_api\src\main.rs
