8c132249f6143a8cf3c8d1b3aa0af2fce1eaf7a4
Salman Khan <salmankkhan733@gmail.com>
2023-11-24 15:36:11 -0500

    Add Error

---
+++ src/error.rs
@@ -0,0 +1,16 @@
+use axum::{http::StatusCode, response::IntoResponse};
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    LoginFail,
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> axum::response::Response {
+        println!("->> {:<12} - {self:?}", "INTO_RES");
+
+        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+    }
+}
